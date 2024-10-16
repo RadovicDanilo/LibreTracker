3810d66a81d2fb6009a413392acc48822acff8ec
Danilo <daniloradovic2003@gmail.com>
2024-10-16 12:54:35 +0200

    make getDailyUsageTimeInMinutes readable

--- app/src/main/java/com/danilor/libretracker/UsageTimeManager.kt
+++ app/src/main/java/com/danilor/libretracker/UsageTimeManager.kt
@@ -9,13 +9,7 @@ import java.time.ZoneId
 object UsageTimeManager {
     data class AppStateModel(
         val packageName: String,
-        var startTime: Long = 0,
-        var endTime: Long = 0,
         var totalTime: Long = 0,
-        var totalCapacity: Double = 0.0,
-        var currentCapacity: Double = 0.0,
-        var isAlreadyResume: Boolean = false,
-        var className: String = "",
         var classMap: HashMap<String, BoolObj> = HashMap()
     )
 
@@ -25,57 +19,38 @@ object UsageTimeManager {
     )
 
     fun getDailyUsageTimeInMinutes(context: Context, date: LocalDateTime): Long {
-        val stateMap = HashMap<String, AppStateModel>()
         val usageStatsManager =
             context.getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager
-
         val startDate =
             date.toLocalDate().atStartOfDay(ZoneId.systemDefault()).toInstant().toEpochMilli()
         val endDate =
             date.toLocalDate().plusDays(1).atStartOfDay(ZoneId.systemDefault()).toInstant()
                 .toEpochMilli()
-
         val excludedPackages = ExcludedPackagesManager.getExcludedPackages()
 
+        val stateMap = HashMap<String, AppStateModel>()
         val eventList = usageStatsManager.queryEvents(startDate, endDate)
+
         while (eventList.hasNextEvent()) {
             val event = UsageEvents.Event()
             eventList.getNextEvent(event)
 
-            if (!excludedPackages.contains(event.packageName)) {
-                if (event.eventType == UsageEvents.Event.ACTIVITY_RESUMED) {
-                    val packageCheck = stateMap[event.packageName]
-                    if (packageCheck != null) {
-                        if (stateMap[event.packageName]!!.classMap[event.className] != null) {
-                            stateMap[event.packageName]!!.className = event.className
-                            stateMap[event.packageName]!!.startTime = event.timeStamp
-                            stateMap[event.packageName]!!.classMap[event.className]!!.startTime =
-                                event.timeStamp
-                            stateMap[event.packageName]!!.classMap[event.className]!!.isResume =
-                                true
-                        } else {
-                            stateMap[event.packageName]!!.className = event.className
-                            stateMap[event.packageName]!!.startTime = event.timeStamp
-                            stateMap[event.packageName]!!.classMap[event.className] =
-                                BoolObj(event.timeStamp, true)
-                        }
-                    } else {
-                        val appStates = AppStateModel(
-                            packageName = event.packageName,
-                            className = event.className,
-                            startTime = event.timeStamp
-                        )
-                        appStates.classMap[event.className] = BoolObj(event.timeStamp, true)
-                        stateMap[event.packageName] = appStates
-                    }
-                } else if (event.eventType == UsageEvents.Event.ACTIVITY_STOPPED) {
-                    val packageCheck = stateMap[event.packageName]
-                    if (packageCheck != null) {
-                        if (stateMap[event.packageName]!!.classMap[event.className] != null) {
-                            stateMap[event.packageName]!!.totalTime += event.timeStamp - stateMap[event.packageName]!!.classMap[event.className]!!.startTime
-                            stateMap[event.packageName]!!.classMap[event.className]!!.isResume =
-                                false
-                        }
+            if (excludedPackages.contains(event.packageName)) continue
+
+            val packageState =
+                stateMap.getOrPut(event.packageName) { AppStateModel(event.packageName) }
+            val classState = packageState.classMap.getOrPut(event.className) { BoolObj(0, false) }
+
+            when (event.eventType) {
+                UsageEvents.Event.ACTIVITY_RESUMED -> {
+                    classState.startTime = event.timeStamp
+                    classState.isResume = true
+                }
+
+                UsageEvents.Event.ACTIVITY_STOPPED -> {
+                    if (classState.isResume) {
+                        packageState.totalTime += event.timeStamp - classState.startTime
+                        classState.isResume = false
                     }
                 }
             }
