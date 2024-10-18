c5003f3a233b5d3e88d2476de573adc359479c2c
Danilo <daniloradovic2003@gmail.com>
2024-10-18 19:52:03 +0200

    Usage per app in ScreenTimeUi

--- app/src/main/java/com/danilor/libretracker/ScreenTimeUI.kt
+++ app/src/main/java/com/danilor/libretracker/ScreenTimeUI.kt
@@ -3,17 +3,29 @@ package com.danilor.libretracker
 //TODO show a list of most used apps
 //TODO button to edit excluded packages
 
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.drawable.BitmapDrawable
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Android
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -21,6 +33,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
@@ -82,6 +95,7 @@ fun ScreenTimeUI(viewModel: ScreenTimeViewModel = viewModel()) {
         }
 
         UsageBarChart(usageByHour = usageInfo?.usageByHour)
+        AppUsageCard(usageByApp = usageInfo?.usageByApp, context = context)
     }
 }
 
@@ -116,4 +130,89 @@ fun UsageBarChart(usageByHour: Array<Int>?) {
         isShowVerticalAxis = true,
         isShowHorizontalLines = true,
     )
+}
+
+@Composable
+fun AppUsageCard(usageByApp: List<AppUsageInfo>?, context: Context) {
+    if (usageByApp == null) return
+
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .height(200.dp)
+            .padding(8.dp),
+        verticalArrangement = Arrangement.spacedBy(4.dp)
+    ) {
+        items(usageByApp.sorted()) { appUsage ->
+
+            val packageName = appUsage.appName
+            val appName = try {
+                val applicationInfo =
+                    context.applicationContext.packageManager.getApplicationInfo(packageName, 0)
+                val label =
+                    context.applicationContext.packageManager.getApplicationLabel(applicationInfo)
+                        .toString()
+                if (label.isEmpty()) packageName else label
+            } catch (e: PackageManager.NameNotFoundException) {
+                e.printStackTrace()
+                packageName
+            }
+
+            val usageTime = appUsage.usageInMinutes
+
+            Card(
+                modifier = Modifier.fillMaxWidth(),
+                shape = MaterialTheme.shapes.medium,
+                elevation = CardDefaults.cardElevation(4.dp)
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    val drawable = try {
+                        context.applicationContext.packageManager.getApplicationIcon(appUsage.appName)
+                    } catch (e: PackageManager.NameNotFoundException) {
+                        e.printStackTrace()
+                        null
+                    }
+
+                    val bitmap = (drawable as? BitmapDrawable)?.bitmap
+
+                    if (bitmap != null) {
+                        Image(
+                            bitmap = bitmap.asImageBitmap(),
+                            contentDescription = null,
+                            modifier = Modifier.size(40.dp)
+                        )
+                    } else {
+                        Icon(
+                            imageVector = Icons.Filled.Android,
+                            contentDescription = null,
+                            modifier = Modifier.size(40.dp),
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.width(8.dp))
+
+                    Column(
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        Text(
+                            text = appName,
+                            style = MaterialTheme.typography.bodyLarge,
+                            color = MaterialTheme.colorScheme.onBackground
+                        )
+                        Text(
+                            text = "${usageTime}m",
+                            style = MaterialTheme.typography.bodySmall,
+                            color = MaterialTheme.colorScheme.onBackground
+                        )
+                    }
+                }
+            }
+        }
+    }
 }
