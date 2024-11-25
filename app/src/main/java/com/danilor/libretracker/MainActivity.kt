76985f64aaac9ba5ccef9bc9737d32e4eb4ec6b0
Danilo <daniloradovic2003@gmail.com>
2024-11-25 20:09:47 +0100

    Added widget update method to main

--- app/src/main/java/com/danilor/libretracker/MainActivity.kt
+++ app/src/main/java/com/danilor/libretracker/MainActivity.kt
@@ -15,19 +15,24 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
 import com.danilor.libretracker.managers.ExcludedPackagesManager
 import com.danilor.libretracker.nav.AppNavigation
 import com.danilor.libretracker.ui.theme.LibreTrackerTheme
 import com.danilor.libretracker.view.RequestPermissionView
+import com.danilor.libretracker.view.widgets.WidgetUpdateWorker
+import java.util.concurrent.TimeUnit
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             LibreTrackerTheme {
+                scheduleWidgetUpdate(applicationContext)
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
                     ExcludedPackagesManager.initialize(applicationContext)
 
@@ -46,12 +51,18 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    fun scheduleWidgetUpdate(context: Context) {
+        val updateRequest =
+            PeriodicWorkRequestBuilder<WidgetUpdateWorker>(1, TimeUnit.MINUTES).build()
+        WorkManager.getInstance(context).enqueueUniquePeriodicWork(
+            "WidgetUpdateWork", ExistingPeriodicWorkPolicy.UPDATE, updateRequest
+        )
+    }
+
     private fun checkUsageStatsPermission(): Boolean {
         val appOps = getSystemService(Context.APP_OPS_SERVICE) as AppOpsManager
         val mode = appOps.checkOpNoThrow(
-            AppOpsManager.OPSTR_GET_USAGE_STATS,
-            android.os.Process.myUid(),
-            packageName
+            AppOpsManager.OPSTR_GET_USAGE_STATS, android.os.Process.myUid(), packageName
         )
         return mode == AppOpsManager.MODE_ALLOWED
     }
